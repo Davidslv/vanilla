9d4bba2a33edb975fb1f58d436849d54f3c1b629
David Silva <davidslv@gmail.com>
2021-07-02 14:57:07 +0100

    Add constant for available algorithms
    ```ruby
    Vanilla::Algorithms::AVAILABLE.each do |algorithm|
      Vanilla.play(rows: 37, columns: 28, algorithm: algorithm, png: true)
    end
    ```

--- lib/vanilla/algorithms.rb
+++ lib/vanilla/algorithms.rb
@@ -4,5 +4,12 @@ module Vanilla
     require_relative 'algorithms/aldous_broder'
     require_relative 'algorithms/recursive_backtracker'
     require_relative 'algorithms/recursive_division'
+
+    AVAILABLE = [
+      Vanilla::Algorithms::AldousBroder,
+      Vanilla::Algorithms::BinaryTree,
+      Vanilla::Algorithms::RecursiveDivision,
+      Vanilla::Algorithms::RecursiveBacktracker,
+    ].freeze
   end
 end
