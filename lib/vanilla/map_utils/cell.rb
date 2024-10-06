4311625cc9e79b3940cbe4058a043887679efe61
David Silva <davidslv@users.noreply.github.com>
2024-10-06 19:04:26 +0100

    Document MapUtils::Cell

--- lib/vanilla/map_utils/cell.rb
+++ lib/vanilla/map_utils/cell.rb
@@ -1,12 +1,24 @@
-# 
-# Represents a single cell in a maze or grid-based map
-# 
-# Each cell has a row and column position, and can be linked to other cells in the map.
-# Cells can also have various properties, such as whether they are a dead end, or contain a player or stairs.
-# The `distances` method calculates the distance from the current cell to all other cells in the map.
-# 
 module Vanilla
   module MapUtils
+    # Represents a single cell in a maze or grid-based map
+    # It has a position in the grid and can be linked to other cells.
+    # Cells can be linked to other cells to form a path or maze.
+    # Cells can also have various properties, such as whether they are a dead end, or contain a player or stairs.
+    #
+    # @example
+    #  cell = Vanilla::MapUtils::Cell.new(row: 0, column: 0)
+    #  cell.north = Vanilla::MapUtils::Cell.new(row: 0, column: 0)
+    #  cell.north.link(cell: cell)
+    #
+    # The `link` method is used to link this cell to another cell.
+    # The `unlink` method is used to unlink this cell from another cell.
+    # The `links` method returns an array of all cells linked to this cell.
+    # The `linked?` method checks if this cell is linked to another cell.
+    # The `neighbors` method returns an array of all neighboring cells (north, south, east, west).
+    # The `distances` method calculates the distance from the current cell to all other cells in the map.
+    # The `dead_end?` method checks if this cell is a dead end.
+    # The `player?` method checks if this cell contains the player.
+    # The `stairs?` method checks if this cell contains stairs.
     class Cell
       attr_reader :row, :column
       attr_accessor :north, :south, :east, :west
@@ -20,59 +32,73 @@ module Vanilla
         @links = {}
       end
 
-      # cell x, y position
+      # Get the position of the cell as an array
+      # @return [Array<Integer>] An array containing the row and column
       def position
         [row, column]
       end
 
+      # Link this cell to another cell
+      # @param cell [Cell] The cell to link to
+      # @param bidirectional [Boolean] Whether to create a bidirectional link
+      # @return [Cell] Returns self for method chaining
       def link(cell:, bidirectional: true)
         @links[cell] = true
         cell.link(cell: self, bidirectional: false) if bidirectional
-
         self
       end
 
+      # Unlink this cell from another cell
+      # @param cell [Cell] The cell to unlink from
+      # @param bidirectional [Boolean] Whether to remove the link in both directions
       def unlink(cell:, bidirectional: true)
         @links.delete(cell)
         cell.unlink(cell: self, bidirectional: false) if bidirectional
       end
 
+      # Get all cells linked to this cell
+      # @return [Array<Cell>] An array of linked cells
       def links
         @links.keys
       end
 
+      # Check if this cell is linked to another cell
+      # @param cell [Cell] The cell to check for a link
+      # @return [Boolean] True if linked, false otherwise
       def linked?(cell)
         @links.key?(cell)
       end
 
+      # Check if this cell is a dead end
+      # @return [Boolean] True if it's a dead end, false otherwise
       def dead_end?
-        !! dead_end
+        !!dead_end
       end
 
+      # Check if this cell contains the player
+      # @return [Boolean] True if it contains the player, false otherwise
       def player?
         tile == Support::TileType::PLAYER
       end
 
+      # Check if this cell contains stairs
+      # @return [Boolean] True if it contains stairs, false otherwise
       def stairs?
         tile == Support::TileType::STAIRS
       end
 
+      # Get all neighboring cells (north, south, east, west)
+      # @return [Array<Cell>] An array of neighboring cells
       def neighbors
-        list = []
-        list << north if north
-        list << south if south
-        list << east if east
-        list << west if west
-
-        list
+        [north, south, east, west].compact
       end
 
+      # Calculate distances from this cell to all other cells in the maze
+      # @return [DistanceBetweenCells] A DistanceBetweenCells object containing distances
       def distances
         distances = Vanilla::MapUtils::DistanceBetweenCells.new(self)
-        frontier = [ self ]
+        frontier = [self]
 
-        # We’re going to keep looping until there are no more cells in the frontier set,
-        # which will mean that we’ve measured the distance of every cell to our root cell.
         while frontier.any?
           new_frontier = []
 
